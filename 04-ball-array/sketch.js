c3ee599d81c04b95d779681b94a9772e807cac5c
Ben <benjamin_hoover@icloud.com>
2026-03-04 15:25:27 -0600

    BALL TELEPORT

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -4,6 +4,7 @@ let ballArray = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noStroke();
 }
 
 function draw() {
@@ -14,21 +15,41 @@ function draw() {
     ball.x += ball.dx;
     ball.y += ball.dy;
 
+    if (ball.x - ball.radius > width){
+      ball.x = -ball.radius;
+    }
+    if (ball.x + ball.radius < 0 ){
+      ball.x = width + ball.radius;
+    }
+    if (ball.y - ball.radius > height){
+      ball.y = -ball.radius;
+    }
+    if (ball.y + ball.radius < 0 ){
+      ball.y = height + ball.radius;
+    }
+    
+
+  
+  
+    fill(ball.r, ball.g, ball.b);
     //display
     circle(ball.x, ball.y, ball.radius * 2);
   }
 }
 function mousePressed() {
-  spaweBall();
+  spaweBall(mouseX, mouseY);
 }
 
-function spaweBall() {
+function spaweBall(_x, _y) {
   let theBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 40),
+    r: random(0, 255),
+    g: random(0, 255),
+    b: random(0, 255),
   };
   ballArray.push(theBall);
 }
