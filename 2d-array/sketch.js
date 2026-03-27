84c73a03fa62fad79d63f7380a9ff88c5d648650
Ben <benjamin_hoover@icloud.com>
2026-03-27 15:22:34 -0600

    created a new icon and loaded some spikes

--- 2d-array/sketch.js
+++ 2d-array/sketch.js
@@ -12,10 +12,7 @@ const GROUND = 1;
 let rows;
 let cols;
 let grid;
-let thePlayer = {
-  x: 0,
-  y: 0,  
-};
+
 
 
 // Image variables
@@ -29,17 +26,35 @@ function preload(){
   backgroundImg = loadImage("gd-background.png");
   blockImg = loadImage("gd-block.png");
   groundImg = loadImage("gd-ground.png");
+  spikeImg = loadImage("gd-spike.png");
+  iconImg = loadImage("gd-cube2.png");
 }
 
 function setup(){
+
+
   createCanvas(windowWidth, windowHeight);
-  image(backgroundImg, 0, 0, width, height);
-  image(blockImg, 0, height - CELL_SIZE * 4, CELL_SIZE, CELL_SIZE);
-  image(groundImg, 0, height - CELL_SIZE * 2, CELL_SIZE * 2, CELL_SIZE * 2);
+  image(backgroundImg, 0, 0, width, height - CELL_SIZE * 3);
+  image(blockImg, CELL_SIZE * 12, height - CELL_SIZE * 4, CELL_SIZE, CELL_SIZE);
+
 }
 
 function draw(){
 
+  let thePlayer = {
+    x: 0,
+    y: height + CELL_SIZE * 8 - 6,
+  };
+
+  for (let i = 0; i < CELL_SIZE * 35; i += CELL_SIZE * 3){
+    image(groundImg, i, height - CELL_SIZE * 3, CELL_SIZE * 3, CELL_SIZE * 3);
+  }
+  for (let j = 0; j < CELL_SIZE * 3; j+= CELL_SIZE){
+    image(spikeImg, j + CELL_SIZE* 13, height - CELL_SIZE * 4, CELL_SIZE, CELL_SIZE);
+  }
+  
+  for (let start = 0; start < 100; start += 10){
+    image(iconImg, thePlayer.x, thePlayer.y, CELL_SIZE, CELL_SIZE);
+    thePlayer.x += start;
+  }
 }
-
-
