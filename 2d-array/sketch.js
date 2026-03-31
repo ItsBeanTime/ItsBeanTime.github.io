64de1516e015193dacd4080dd00975b79a5d8974
Ben <benjamin_hoover@icloud.com>
2026-03-31 15:25:22 -0600

    changing my code to use a txt file as the level

--- 2d-array/sketch.js
+++ 2d-array/sketch.js
@@ -8,9 +8,10 @@
 // Constants
 const CELL_SIZE = 64;
 
+let lines;
+let level;
 let rows;
 let cols;
-let grid;
 
 // Image variables
 let iconImg;
@@ -20,6 +21,9 @@ let backgroundImg;
 let spikeImg;
 
 function preload(){
+  level = "assets/level.txt";
+  lines = loadStrings(level);
+
   backgroundImg = loadImage("gd-background.png");
   blockImg = loadImage("gd-block.png");
   groundImg = loadImage("gd-ground.png");
@@ -28,25 +32,9 @@ function preload(){
 }
 
 function setup(){
-  
-
   createCanvas(windowWidth, windowHeight);
   rows = Math.floor(height);
   cols = Math.floor(width);
-  grid = [[],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [2,2,2,2,2,0,2,2,2,2],
-    [2,2,2,2,2,1,2,1,2,2],
-
-
-
-  ];
 
   image(backgroundImg, 0, 0, width, height - CELL_SIZE * 3);
 
@@ -56,19 +44,7 @@ function draw(){
   for (let i = 0; i < CELL_SIZE * 35; i += CELL_SIZE * 3){
     image(groundImg, i, height - CELL_SIZE * 3.5, CELL_SIZE * 3.5, CELL_SIZE * 3.5);
   }
-  spawnSpike();
+  image(iconImg, 0, height - CELL_SIZE * 4.5, CELL_SIZE, CELL_SIZE);
 }
 
-function spawnSpike(){
-  for (let y = 0; y < rows; y++){
-    for (let x = 0; x < cols; x++){
-      if (grid[y][x] === 0){
-        image(spikeImg, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-      }
-      else if (grid[y][x] === 1){
-        image(blockImg, x * CELL_SIZE, y * CELL_SIZE , CELL_SIZE, CELL_SIZE);
-      }
-    }
-  }
-}
 
